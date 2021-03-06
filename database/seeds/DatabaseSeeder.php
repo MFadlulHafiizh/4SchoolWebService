f5109de947f7fc1fc0f84430d10f8626175323cb
Deadmandream <azkazaki11@gmail.com>
2021-03-06 21:18:12 +0700

    Menambah Read semua gedung kecuali gedung A

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Ruangan;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // 
+        $this->call(RoomTableSeeder::class);
+
     }
 }
