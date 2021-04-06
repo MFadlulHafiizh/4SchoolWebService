9e949c12ac83e6b946ce8ac810b0583e9905e3ff
vanhan200902 <vanhan200902@gmail.com>
2021-04-06 12:15:12 +0700

    get data guru classroom

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,8 @@ Route::post('refresh', 'AuthController@refresh');
 
 Route::group(['middleware'=> 'api.role:guru'], function() {
     Route::post('GuruSchedule', 'api\GuruInteractionController@GuruSchedule');
+    Route::post('GuruSchedule/create_tugas/{id_kelas}/{id_matpel}', 'api\GuruInteractionController@tugas_kelas');
+    Route::post('GuruSchedule/index_classroom_guru/{id_kelas}', 'api\GuruInteractionController@IndexClassroom');    
 });
 Route::group(['middleware'=> 'api.role:siswa'], function() {
     Route::get('SiswaSchedule', 'api\SiswaInteractionController@SiswaSchedule');
@@ -39,8 +41,3 @@ Route::patch('upload/{id}', 'api\UploadController@uploadImageDecoded');
 Route::get('get-image', 'api\UploadController@getPhoto');
 Route::get('classRoomData', 'api\SiswaInteractionController@ClassRoomIndex');
 
-Route::post('GuruSchedule/create_tugas/{id_kelas}/{id_matpel}', 'api\GuruInteractionController@tugas_kelas');
-
-
-
-Route::post('GuruSchedule/index_classroom_guru/{id_kelas}', 'api\GuruInteractionController@IndexClassroom');
