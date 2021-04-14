eb681420d3ec846fa32d07b034d574399bddd0c3
M Fadlul Hafiizh <muh.hafiizh56@gmail.com>
2021-04-14 13:04:03 +0700

    before pull

--- routes/api.php
+++ routes/api.php
@@ -31,14 +31,15 @@ Route::post('refresh', 'AuthController@refresh');
 Route::group(['middleware'=> 'api.role:guru'], function() {
     Route::post('GuruSchedule', 'api\GuruInteractionController@GuruSchedule');
     Route::post('GuruSchedule/create_tugas/{id_kelas}/{id_matpel}', 'api\GuruInteractionController@tugas_kelas');
-    Route::post('GuruSchedule/index_classroom_guru/{id_kelas}', 'api\GuruInteractionController@IndexClassroom');    
+    Route::get('GuruSchedule/index_classroom_guru/{id_kelas}', 'api\GuruInteractionController@IndexClassroom');    
 });
 Route::group(['middleware'=> 'api.role:siswa'], function() {
     Route::post('SiswaSchedule', 'api\SiswaInteractionController@SiswaSchedule');
+    Route::get('SiswaSchedule/classRoomData', 'api\SiswaInteractionController@ClassRoomIndex');
 });
 Route::get('classInfo', 'api\ClassroomController@getInfoClass');
 Route::patch('upload/{id}', 'api\UploadController@uploadImageDecoded');
 Route::get('get-image', 'api\UploadController@getPhoto');
-Route::get('classRoomData', 'api\SiswaInteractionController@ClassRoomIndex');
+
 Route::get('getHelp', 'api\BaseController@getHelp');
 
