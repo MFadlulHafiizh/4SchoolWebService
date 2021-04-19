c85a38d8326aa14c9bd9be8593866c5e96e0ab0b
vanhan200902 <vanhan200902@gmail.com>
2021-04-19 11:23:44 +0700

    Update Profile & get data kelas where tingkatan

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,10 @@ Route::get('getHelp', 'api\BaseController@getHelp');
 
 Route::post('GuruSchedule/create_tugas/{id_jadwal}', 'api\GuruInteractionController@tugas_kelas');
 
+
+Route::post('Update_Profile/{id_user}', 'api\SiswaInteractionController@updateprofile'); 
+Route::post('ShowKelas', 'api\SiswaInteractionController@showkelas'); 
+
 //========================================
 //Forgot Reset Password
 //========================================
